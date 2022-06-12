7524d9e05bb01eea545ff7f19d9eb66dbb2cb6e0
Leonarda Prawns <leonarda@ga-p6.msk.21-school.ru>
2022-06-12 14:06:17 +0300

    Fibonacci done

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
-int fibonacci(int N)
-{
-if (N == 1 || N == 2)
- {
- return 1;
- }
- else
- {
- return fibonacci(N - 1) + fibonacci(N - 2);
- }
- 
+int fibonacci(int N) {
+if (N == 1 || N == 2) {
+    return 1;
+} else {
+    return fibonacci(N - 1) + fibonacci(N - 2);
 }
-int main()
-{
- int N;
- printf("N=");
- scanf("%d", &N);
- printf("%d ", fibonacci(N));
- getchar(); getchar();
- return 0;
+}
+int main() {
+    int N;
+    printf("N=");
+    scanf("%d", &N);
+    printf("%d ", fibonacci(N));
+    getchar(); getchar();
+    return 0;
 }
